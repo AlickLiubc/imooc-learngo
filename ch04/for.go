9c32940e328d44f511fc1e44ed2ac4cb5709e71d
AlickLiubc <nrlbc@sina.com>
2023-11-19 15:22:39 +0800

    for range 学习

--- ch04/for.go
+++ ch04/for.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	// for 循环
 	/*
-	for init; condition; post {}
-	 */
+		for init; condition; post {}
+	*/
 	//var i int
 	//for ; i < 3; i++  {
 	//	fmt.Println(i)
@@ -29,10 +29,31 @@ func main() {
 
 	for i := 1; i < 10; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Print(strconv.Itoa(j) + "*" +strconv.Itoa(i) + "=" + strconv.Itoa(i * j))
+			fmt.Print(strconv.Itoa(j) + "*" + strconv.Itoa(i) + "=" + strconv.Itoa(i*j))
 			fmt.Print("\t")
 		}
 		fmt.Println()
 	}
+
+	// for 循环还有一种用法，for range，主要就是对字符串、数组、切片等
+	/*
+		for key, value := range {
+
+		}
+	*/
+	name := "imooc go"
+	for key, value := range name {
+		// fmt.Println(key, value)
+		fmt.Printf("%d, %c\r\n", key, value)
+	}
+
+	/*
+		字符串：key代表字符串索引,value是值的拷贝
+		数组
+		切片
+		map
+		channel
+	*/
+	// for range key, value
 
 }
