1033d2d80fed1e4574797d0cab4b6efbbafe0826
AlickLiubc <nrlbc@sina.com>
2023-11-20 01:11:21 +0800

    '数组的初始化'

--- ch05/main.go
+++ ch05/main.go
@@ -3,9 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	var course1 [3]string
-	var course2 [4]string
+	// go语言提供了哪些集合类型的数据结构，
+	// 数组，切片(slice)，map，list
+	// 数组 定义：var name [count]int
 
-	fmt.Printf("%T\r\n", course1)
-	fmt.Printf("%T", course2)
+	// 只有3个元素的数组
+	// var course1 [3]string
+
+	// []string [3]string 是2种不同的类型
+	// 只有4个元素的数组
+	// var course2 [4]string
+
+	//course1[0] = "go"
+	//course1[1] = "grpc"
+	//course1[2] = "gin"
+
+	// fmt.Println(course1)
+	// fmt.Printf("%T\r\n", course1)
+	// fmt.Printf("%T", course2)
+
+	//for _, value := range course1 {
+	//	fmt.Println(value)
+	//}
+
+	// 数组的初始化
+	// course1 := [3]string{"go", "grpc", "gin"}
+
+
+	//course1 := [3]string{2: "grpc"}
+	//for _, value := range course1 {
+	//	fmt.Println(value)
+	//}
+
+	course1 := [...]string{"go", "grpc", "gin"}
+	for _, value := range course1 {
+		fmt.Println(value)
+	}
+
+	for i := 0; i < len(course1); i++ {
+		fmt.Println(course1[i])
+	}
 }
