a339eaf12e1ef074087a29e40abd0b4d2b7839d2
AlickLiubc <nrlbc@sina.com>
2023-11-22 00:17:16 +0800

    '切片的多种初始化方式'

--- ch05/slice/main.go
+++ ch05/slice/main.go
@@ -15,4 +15,22 @@ func main()  {
 
 	fmt.Println(courses[1])
 
+	// 切片的初始化 3种
+	// 1：从数组直接创建
+	// 2：使用string{}
+	// 3：make
+	allCourses := [5]string{"go", "grpc", "gin", "mysql", "elasticsearch"}
+	// 左闭右开的区间
+	courseSlice := allCourses[0:len(allCourses)]
+	fmt.Println(courseSlice)
+
+	// courseSlice := []string{"go", "grpc", "gin", "mysql", "elasticsearch"}
+
+	// make 函数
+	allCourses2 := make([]string, 3)
+	allCourses2[0] = "c"
+	fmt.Println(allCourses2)
+
+	var allCourse3 []string
+	allCourse3[0] = "c"
 }
