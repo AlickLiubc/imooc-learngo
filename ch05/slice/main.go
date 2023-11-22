409976a80933db4198b5315876a43370c0ec201c
AlickLiubc <nrlbc@sina.com>
2023-11-22 23:32:12 +0800

    '切片的数据访问'

--- ch05/slice/main.go
+++ ch05/slice/main.go
@@ -20,17 +20,31 @@ func main()  {
 	// 2：使用string{}
 	// 3：make
 	allCourses := [5]string{"go", "grpc", "gin", "mysql", "elasticsearch"}
-	// 左闭右开的区间
-	courseSlice := allCourses[0:len(allCourses)]
-	fmt.Println(courseSlice)
+	//// 左闭右开的区间
+	//courseSlice := allCourses[0:len(allCourses)]
+	//fmt.Println(courseSlice)
+	//
+	//// courseSlice := []string{"go", "grpc", "gin", "mysql", "elasticsearch"}
+	//
+	//// make 函数
+	//allCourses2 := make([]string, 3)
+	//allCourses2[0] = "c"
+	//fmt.Println(allCourses2)
+	//
+	//var allCourse3 []string
+	//allCourse3[0] = "c"
 
-	// courseSlice := []string{"go", "grpc", "gin", "mysql", "elasticsearch"}
+	// 访问切片的元素，可以单个，也可以多个
+	fmt.Println(allCourses[1])
 
-	// make 函数
-	allCourses2 := make([]string, 3)
-	allCourses2[0] = "c"
-	fmt.Println(allCourses2)
-
-	var allCourse3 []string
-	allCourse3[0] = "c"
+	// [start:end]
+	/*
+	1. 如果只有start 没有end 就表示从start开始到结尾的所有数据
+	2. 如果没有start 有end 就表示从0到end之前的所有数据
+	3. 如果有start 没有end
+	4. 有start 有end
+	*/
+	fmt.Println(allCourses[:])
+	fmt.Println(allCourses[1:])
+	fmt.Println(allCourses[1:3])
 }
