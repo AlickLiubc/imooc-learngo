9113c14d7362e1dcb76bc028250d02d611a542cd
AlickLiubc <nrlbc@sina.com>
2023-11-25 21:25:27 +0800

    基本类型的转换和format格式转换

--- ch02/convert/main.go
+++ ch02/convert/main.go
@@ -5,17 +5,17 @@ import (
 	"strconv"
 )
 
-func main()  {
+func main() {
 	var a int8 = 12
 	//var b = uint8(a)
-	//
+
 	//var f float32 = 3.14
 	//var c = int32(f)
 
 	var f64 = float64(a)
 	fmt.Println(f64)
 
-	//type IT int
+	//type IT int	// 类型要求很严格
 	//var c IT = IT(a)
 
 	// 字符串转数字
@@ -26,10 +26,33 @@ func main()  {
 	}
 	fmt.Println(myint)
 
-
 	var myi = 32
 	fmt.Println(strconv.Itoa(myi))
 
 	// 字符串转化为float32,转换为bool
-	fmt.Println(strconv.ParseFloat("3.1415", 64))
+	float, err := strconv.ParseFloat("3.1415", 64)
+	if err != nil {
+		fmt.Println(float)
+	}
+
+	parseInt, err := strconv.ParseInt("12", 8, 64)
+	if err != nil {
+		return
+	}
+	fmt.Println(parseInt)
+
+	parseBool, err := strconv.ParseBool("0")
+	if err != nil {
+		fmt.Println("ParseBool error")
+	}
+	fmt.Println(parseBool)
+
+	// 基本类型转字符串
+	boolStr := strconv.FormatBool(true)
+	fmt.Println(boolStr)
+
+	floatStr := strconv.FormatFloat(3.1415926, 'f', -1, 64)
+	fmt.Println(floatStr)
+
+	fmt.Println(strconv.FormatInt(42, 16))
 }
--- ch02/main.go
+++ ch02/main.go
@@ -2,31 +2,33 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	//var a int8
 	//var b int16
 	//var c int32
 	//var d int64
-	//
+
 	//var ua uint8
 	//var ub uint16
 	//var uc uint32
 	//var ud uint64
-	//
-	//var e int // 动态类型, 用的时候就会指定，用起来挺麻烦的
-	//
+
+	//var e int // 动态类型, 用的时候就会知道，用起来挺麻烦的
+
 	//a = int8(b)
-	//
+
 	//var f1 float32	// 大约是3.4e38
 	//var f2 float64	// 1.8e308
-	//
+
 	//f1 = 3
 	//f2 = 3.14
 
-	//var c byte
+	//var c byte	// 主要用于存放字符，存放字符串
 	//c = 'a'
 	c1 := 97 + 1
-	fmt.Printf("c=%c", c1)
+	fmt.Printf("c=%c\r\n", c1)
+
+	// var c2 rune // 也是字符
 
 	var name string
 	name = "I am bobby"
