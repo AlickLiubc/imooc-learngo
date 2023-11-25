3b3f4597d1d0e35c65f88f3b43234755e95463e9
AlickLiubc <nrlbc@sina.com>
2023-11-25 13:25:10 +0800

    变量常量的定义与使用代码补充

--- ch01/const/main.go
+++ ch01/const/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	const PI = 3.1415926
-	//const MY_NAME
+	// 常量，定义的时候就指定的值，不能修改，常量尽量全部大写
+	// 显示定义
+	const PI float32 = 3.1415926
+	// const MY_NAME
 	const (
 		UNKNOWN = 1
-		FEMALE = 2
-		MALE = 3
+		FEMALE  = 2
+		MALE    = 3
 	)
 
 	const (
@@ -19,5 +21,10 @@ func main() {
 		m
 	)
 
+	/*
+		常量类型只可以定义bool，数值(整数，浮点数和复数)和字符串
+		不曾使用的常量，没有强制使用的要求
+		显示指定类型的时候，必须确保常量左右值类型一致
+	*/
 	fmt.Println(x, y, s, z, m)
 }
--- ch01/main.go
+++ ch01/main.go
@@ -9,8 +9,8 @@ import "fmt"
 
 var (
 	name = "bobby"
-	age = 16
-	ok bool
+	age  = 16
+	ok   bool
 )
 
 func main() {
@@ -20,6 +20,21 @@ func main() {
 	// var name = 1
 
 	age := 1
+	var age2 int
+	var name2 string
 
 	fmt.Println(age)
+	fmt.Println(age2)
+	fmt.Println(name2)
+
+	// 多变量定义
+	var user1, user2, user3 = "bobby1", 1, "bobby3"
+	fmt.Println(user1, user2, user3)
+
+	// 注意：
+	// 变量必须先定义才能使用
+	// go语言是静态语言，要求变量的类型和赋值类型一致
+	// 变量名不能冲突
+	// 简洁变量定义不能用于全局变量
+	// 变量是有零值的
 }
