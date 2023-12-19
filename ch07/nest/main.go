af559ebf11d99abac72c2887070f604e719c7b62
AlickLiubc <nrlbc@sina.com>
2023-12-19 23:18:00 +0800

    '结构体定义方法'

--- ch07/nest/main.go
+++ ch07/nest/main.go
@@ -16,17 +16,37 @@ type Student struct {
 	score float32
 }
 
+// 接收器有2种形态
+//func (p Person) print() {
+func (p *Person) print() {
+	// 有可能该函数中想要改变p的值，就是person对象很大，
+	// 数据较大的时候，考虑使用指针方式传递
+	p.age = 20
+	fmt.Printf("name: %s, age: %d\r\n", p.name, p.age)
+}
+
+// func (s sturctType) funcName(param1 param1Type,...)(returnVal1 returnType1,...){}
+
 func main() {
+	p := Person{
+		"bobby", 18,
+	}
+	p.print()
+
+	fmt.Println(p.age)
+
 	//s := Student {
 	//	Person {
-	//		"bobby", 18,
+	//		"bobby", 19,
 	//	},
 	//	95.6,
 	//}
+	//
+	//s.print()
 
-	s := Student{}
+	// s := Student{}
 	// s.p.name = "bobby"
 	// fmt.Println(s.p.age)
-	fmt.Println(s.age)
+	// fmt.Println(s.age)
 }
 
