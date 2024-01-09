1155fbc8589cc406a1dd36098d8eaa8b8269a6ce
AlickLiubc <nrlbc@sina.com>
2024-01-10 00:43:57 +0800

    '补充slice演示代码'

--- ch05/how/main.go
+++ ch05/how/main.go
@@ -16,8 +16,32 @@ func printSlice(data []string) {
 func main()  {
 	// go的slice在函数参数传递的时候是值传递，还是引用传递
 	// 值传递，效果又呈现了引用传递
-	courses := []string{"go", "grpc", "gin"}
-	printSlice(courses)
+	//courses := []string{"go", "grpc", "gin"}
+	//printSlice(courses)
+	//
+	//fmt.Println(courses)
+	//
+	//courses = append(courses, "abc")
 
-	fmt.Println(courses)
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9 ,10}
+	s1 := data[1:6]
+	s2 := data[2:7]
+
+	s2 = append(s2, 1, 2, 3, 4, 5 ,6 ,7, 8, 9, 1, 1, 1, 1)
+	s2[0] = 22
+
+	fmt.Println(len(s1), cap(s1))
+	fmt.Println(len(s2), cap(s2))
+
+	fmt.Println(s2)
+	fmt.Println(s1)
+
+	var dataInt []int
+	for i := 1; i < 2000; i++ {
+		dataInt = append(dataInt, i)
+
+		fmt.Printf("len:%d, cap:%d\r\n", len(dataInt), cap(dataInt))
+	}
+
+
 }
