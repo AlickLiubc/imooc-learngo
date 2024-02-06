e4f489b9dc02a1844fccf6c2fac0f308762e395d
AlickLiubc <nrlbc@sina.com>
2024-02-06 23:29:01 +0800

    WaitGroup的用法

---
+++ ch12/wg/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 子goroutine怎么通知到主的goroutine
+// 主的goroutine怎么知道子的goroutine已经结束了
+
+func main() {
+	var wg sync.WaitGroup
+
+	// 我要监控多少个gorountine执行结束
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			defer wg.Done()
+			fmt.Println(i)
+		}(i)
+	}
+
+	// 等到
+	wg.Wait()
+
+	fmt.Println("all done")
+
+	// waitgroup主要用于goroutine的执行等待
+	// Add()需要和Done()配套使用
+}
