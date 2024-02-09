ec146a5a1b440a43e5ca20990547bd591eff4198
AlickLiubc <nrlbc@sina.com>
2024-02-09 17:19:04 +0800

    mutex和atomic学习

---
+++ ch12/lock/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var total int32
+var wg sync.WaitGroup
+
+// var lock sync.Mutex
+
+// lock2 := lock
+// 锁能复制么？
+// 复制后就失去了锁的效果了
+func add() {
+	defer wg.Done()
+	for i := 0; i < 100000; i++ {
+		//lock.Lock()
+		// 竞争
+		//total += 1
+		//lock.Unlock()
+		atomic.AddInt32(&total, 1)
+	}
+}
+
+func sub() {
+	defer wg.Done()
+	for i := 0; i < 100000; i++ {
+		//lock.Lock()
+		//total -= 1
+		//lock.Unlock()
+		atomic.AddInt32(&total, -1)
+	}
+}
+
+func main() {
+	wg.Add(2)
+	go add()
+	go sub()
+	wg.Wait()
+	fmt.Println(total)
+}
