0546cd70b484127d6b9396f2aa2c00a32095bbe2
AlickLiubc <nrlbc@sina.com>
2024-02-19 00:52:08 +0800

    修改代码注释

--- ch12/wg/main.go
+++ ch12/wg/main.go
@@ -5,13 +5,13 @@ import (
 	"sync"
 )
 
-// 子goroutine怎么通知到主的goroutine
+// 子goroutine怎么通知到主的goroutine自己结束了
 // 主的goroutine怎么知道子的goroutine已经结束了
 
 func main() {
 	var wg sync.WaitGroup
 
-	// 我要监控多少个gorountine执行结束
+	// 我要监控多少个goroutine执行结束
 	wg.Add(100)
 	for i := 0; i < 100; i++ {
 		go func(i int) {
